5467c8091bf6156283f221d8673531473ae142e2
a6hinavram <a6hinavram@gmail.com>
2020-12-16 16:09:00 +0530

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import {MatSelectModule} from '@angular/material/select';
 import {MatNativeDateModule} from '@angular/material/core';
+
+import {DemoMaterialModule} from './material-module';
+
 
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
-    MatSelectModule,
+    ReactiveFormsModule,
+    DemoMaterialModule,
     MatNativeDateModule
   ],
   declarations: [
